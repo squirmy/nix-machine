c80667bfb093fd49a67ca12b0d8234bcda322df3
Adam Woods <squirmy.dev@gmail.com>
2024-02-17 14:05:22 +0800

    chore: typo

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     flakeModule = ./flake-module.nix;
 
     templates = {
-      trivial = {
+      minimal = {
         path = ./templates/minimal;
         description = "A basic flake demonstrating the usage of nix-machine in one file";
       };
