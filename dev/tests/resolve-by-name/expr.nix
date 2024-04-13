6981bd69626cb507460634afbbe75769c0bd0edc
Adam Woods <squirmy.dev@gmail.com>
2024-04-13 17:05:11 +1000

    chore: add additional test for resolve

---
+++ dev/tests/resolve-by-name/expr.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  flakeLib,
+}: let
+  config = flakeLib.config.resolve {
+    path = ../_fixture/by-name-config;
+    scheme = "by-name";
+  };
+
+  testOptions = {lib, ...}: {
+    options.names = lib.mkOption {
+      type = lib.types.lines;
+    };
+  };
+
+  evalDarwin = lib.evalModules {
+    modules = [testOptions] ++ config.darwin;
+  };
+  evalHome = lib.evalModules {
+    modules = [testOptions] ++ config.home;
+  };
+in {
+  darwin = {
+    inherit (evalDarwin) options config;
+  };
+  home = {
+    inherit (evalHome) options config;
+  };
+}
---
+++ dev/tests/resolve-provided/expr.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  flakeLib,
+}: let
+  config = flakeLib.config.resolve {
+    options = [../_fixture/provided-config/options.nix];
+    darwin = [../_fixture/provided-config/nix-darwin.nix];
+    home = [../_fixture/provided-config/home-manager.nix];
+    path = null;
+  };
+
+  testOptions = {lib, ...}: {
+    options.names = lib.mkOption {
+      type = lib.types.lines;
+    };
+  };
+
+  evalDarwin = lib.evalModules {
+    modules = [testOptions] ++ config.darwin;
+  };
+  evalHome = lib.evalModules {
+    modules = [testOptions] ++ config.home;
+  };
+in {
+  darwin = {
+    inherit (evalDarwin) options config;
+  };
+  home = {
+    inherit (evalHome) options config;
+  };
+}
