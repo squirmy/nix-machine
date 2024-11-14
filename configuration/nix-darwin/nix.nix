e3fdbac1560e6bbe320e6ebbfdd5a25144f233c4
Adam Woods <squirmy.dev@gmail.com>
2024-11-14 23:11:39 +1100

    chore: order substitutors

--- configuration/nix-darwin/nix.nix
+++ configuration/nix-darwin/nix.nix
@@ -27,7 +27,7 @@
     # https://github.com/NixOS/nix/issues/7273
     auto-optimise-store = lib.mkDefault false;
 
-    substituters = ["https://nix-community.cachix.org"];
+    substituters = ["https://cache.nixos.org/" "https://nix-community.cachix.org"];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 
