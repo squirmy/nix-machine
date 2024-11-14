5f33de9593c6909265f1bcfa51ae50b358123a51
Adam Woods <squirmy.dev@gmail.com>
2024-11-14 23:09:16 +1100

    chore: add nix-community to substitutors

--- configuration/nix-darwin/nix.nix
+++ configuration/nix-darwin/nix.nix
@@ -27,7 +27,7 @@
     # https://github.com/NixOS/nix/issues/7273
     auto-optimise-store = lib.mkDefault false;
 
-    trusted-substituters = ["https://nix-community.cachix.org"];
+    substituters = ["https://nix-community.cachix.org"];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 
